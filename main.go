0c8ac7255ddcb1429e78629c8b9e3843e0004587
davidjost <github@spitzenfoto.com>
2022-08-19 11:56:44 +0200

    lesson 11 structs with functions

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
-import (
-	"log"
-	"time"
-)
+import "log"
 
-type User struct {
+/*
+https://www.tutorialspoint.com/go/go_structures.htm
+Structures are used to represent a record. Suppose you want to keep track of the books in a library. You might want to track the following attributes of each book −
+
+		Title
+		Author
+		Subject
+		Book ID
+
+In such a scenario, structures are highly useful.
+*/
+type myStruct struct {
 	FirstName string
-	LastName string
-	PhoneNumber string
-	Age int
-	BirthDate time.Time
+}
+
+// assigning functions to structs, called a receiver, it ties the function to the struct myStruct
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
 }
 
 func main() {
-	user := User {
-		FirstName: "Trevor",
-		LastName: "Philips",
+	// single struct value assignment
+	var myVar myStruct
+	myVar.FirstName = "John"
+
+	// shorthand struct value assignment
+	myVar2 := myStruct{
+		FirstName: "Mary",
 	}
 
-	log.Println(user.FirstName, user.LastName, user.BirthDate)
+	// myVar is set to the myStruct type, which also has the function attached to it. It's possible to write logic for the data in the struct attached to the same struct, which groups source and manipulation nicely.
+	log.Println("myVar is set to", myVar.printFirstName())
+	log.Println("myVar2 is set to", myVar2.printFirstName())
 }

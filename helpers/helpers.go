5d6b9a7e1c71dfbab096fcdce789a17cb63702d0
davidjost <github@spitzenfoto.com>
2022-08-21 22:58:46 +0200

    lesson 17 channels

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,13 @@
 package helpers
 
-type SomeType struct {
-	TypeName   string
-	TypeNumber int
+import (
+	"math/rand"
+	"time"
+)
+
+func GenRandNum(number int) int {
+	// we need to seed a new number to the rand function, we take the unix timecode for that
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(number)
+	return value
 }
--- main.go
+++ main.go
@@ -6,10 +6,30 @@ import (
 	"github.com/davidjost/go-lesson-package/helpers"
 )
 
+const numPool = 1000
+
+
+// func function_name( [parameter list] ) [return_types]
+// the parameter this receives is bleh, of type channel, that channel only takes int as types.
+func CalcValue(bleh chan int) {
+	// write random number to randNum
+	randNum := helpers.GenRandNum(numPool)
+	// push randNum to the channel that comes from the parameter of the function
+	bleh <- randNum
+}
+
 func main() {
-	log.Println("test")
+	// channels lesson
 
-	var myVar helpers.SomeType
+	// open channel
+	intChan := make(chan int)
+	// closing a channel after use is required best practice
+	defer close(intChan)
 
-	
+	// create extra go routine for concurrent execution
+	go CalcValue(intChan)
+
+	// num gets assigned whatever is coming out of the channel
+	num := <-intChan
+	log.Println("num is", num)
 }

3ea9cbfdcb38a3d8e2146760e441dc4c1522a906
davidjost <github@spitzenfoto.com>
2022-08-21 20:09:24 +0200

    lesson 12 slices

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import "log"
 
-type User struct {
-	FirstName string
-	LastName string
-}
-
 func main() {
-	// maps have to be set up with the shorthand syntax since a simple declaration without assignment results in a nil map. Nil maps cannot take any assignments.
+	// slices are like arrays
+	var mySlice []string
+	
+	mySlice = append(mySlice, "James")
+	mySlice = append(mySlice, "Will")
+	mySlice = append(mySlice, "Mary")
 
-	// map name := make(type[key type]value type)
-	myMap := make(map[string]User)
-
-	me := User {
-		FirstName: "Trevor",
-		LastName: "Philips",
-	}
-
-	myMap["me"] = me
-
-	log.Println(myMap)
+	log.Println(mySlice)
 }

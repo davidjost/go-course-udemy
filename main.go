37da5f4b65a21d43dfdaf32829b114ba95176cd1
davidjost <github@spitzenfoto.com>
2022-08-21 20:23:04 +0200

    lesson 12 more slices

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	// slices are like arrays
-	var mySlice []string
+	var mySlice []int
 	
-	mySlice = append(mySlice, "James")
-	mySlice = append(mySlice, "Will")
-	mySlice = append(mySlice, "Mary")
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
 
-	log.Println(mySlice)
+	// sorting slices
+	sort.Ints(mySlice)
+
+	// log.Println(mySlice)
+
+	// shorthand slices
+	numbers := []int{1,2,3,4,5,6,7,8,9,10}
+
+	// slices like arrays start at 0, to limit the output use [:]
+	// log.Println(numbers[4:10])
+
+	// names := []string{"william", "jones", "pete"}
+
+	// log.Println(names[1:2])
+
+	// var numbers = make([]int,3,5)
+  printSlice(numbers)
+}
+
+func printSlice(x []int){
+	fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
 }

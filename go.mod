e34484edf1cab4718395a9da49cf0ae2dd66a599
davidjost <github@spitzenfoto.com>
2022-08-22 13:50:17 +0200

    lesson 23 functions and handlers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-course-udemy
+
+go 1.19
--- main.go
+++ main.go
@@ -5,16 +5,32 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(write http.ResponseWriter, request *http.Request){
+	fmt.Fprintf(write, "This is the home page")
+}
+
+// About is the about page handler
+func About(write http.ResponseWriter, request *http.Request){
+	sum := addValues(2, 2)
+	// the underscores are used to ignore expected values to get the program to run
+	_, _ = fmt.Fprintf(write, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// lower case function names are like private funcs, only visible to the current package
+func addValues(x, y int) int {
+	return x + y 
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		n, err := fmt.Fprintf(writer, "Hey web")
+	
+	// basic routing, for / call function Home
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(fmt.Sprintf("Number of Bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application server on port %s", portNumber))
+	
+	_ = http.ListenAndServe(portNumber, nil)
 }

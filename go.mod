f762cbc8bc09de418f9091cb34fd3bfdc56c644d
davidjost <github@spitzenfoto.com>
2022-08-22 12:50:40 +0200

    lesson 21 first web server

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/davidjost/go-lesson-package
-
-go 1.19
--- main.go
+++ main.go
@@ -1,30 +1,20 @@
 package main
 
 import (
-	"errors"
-	"log"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-	result, err := divide(100.0, 0)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		n, err := fmt.Fprintf(writer, "Hey web")
 
-	if err != nil {
-		log.Println(err)
-		return
-	}
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	log.Println("result of division is", result)
-}
+		fmt.Println(fmt.Sprintf("Number of Bytes written: %d", n))
+	})
 
-func divide(x, y float32) (float32, error) {
-	var result float32
-
-	if y == 0 {
-		return result, errors.New("cannot divide by 0")
-	}
-
-	result = x / y
-
-	// return the result and nil for the error
-	return result, nil
+	_ = http.ListenAndServe(":8080", nil)
 }
